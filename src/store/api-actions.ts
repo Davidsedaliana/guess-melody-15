konovalov-as/guess-melody-10

7.21. Добавит middleware и добавит новое действие.
Мы сконфигурировали историю и теперь надо понять как будем выполнять
перенаправление. Самый простой способ: подключить наш объект в нужное
место и при необходимости вызывать метод `push`.

Способ рабочий, но не очень удобен. Было бы здорово завести для этого
отдельное действие и использовать точно также как и другие действия.

Чтобы добиться такого поведения, создадим новую middleware, которую
назовём `redirect`, а затем подключим её в список middleware при
конфигурировании хранилища.

После этого добавим новое действие — `redirectToRoute` и сразу опишем
для него функцию (в соответствии с паттерном «action creator»). Напомним,
при выполнении этого действия, будет выполняться перенаправление
пользователя на нужный маршрут.

После описания действия, обновим асинхронное действие `loginAction`.
В случае успешной аутентификации перенаправим пользователя на маршрут
`/result`.

project/src/store/api-actions.ts
@@ -2,9 +2,9 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Questions} from '../types/question';
-import {loadQuestions, requireAuthorization, setDataLoadedStatus, setError} from './action';
+import {loadQuestions, requireAuthorization, setDataLoadedStatus, setError, redirectToRoute} from './action';
 import {saveToken, dropToken} from '../services/token';
-import {APIRoute, AuthorizationStatus, TIMEOUT_SHOW_ERROR} from '../const';
+import {APIRoute, AuthorizationStatus, AppRoute, TIMEOUT_SHOW_ERROR} from '../const';
 import {AuthData} from '../types/auth-data';
 import {UserData} from '../types/user-data';
 import {store} from './';
@@ -59,6 +59,7 @@ export const loginAction = createAsyncThunk<void, AuthData, {
     const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
     saveToken(token);
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(redirectToRoute(AppRoute.Result));
   },
 );
 

Davidsedaliana/guess-melody-15

src/store/api-actions.ts
@@ -2,9 +2,9 @@ import {AxiosInstance} from 'axios';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AppDispatch, State} from '../types/state.js';
 import {Questions} from '../types/question';
-import {loadQuestions, requireAuthorization, setQuestionsDataLoadingStatus, setError} from './action';
+import {loadQuestions, requireAuthorization, setQuestionsDataLoadingStatus, setError, redirectToRoute} from './action';
 import {saveToken, dropToken} from '../services/token';
-import {APIRoute, AuthorizationStatus, TIMEOUT_SHOW_ERROR} from '../const';
+import {APIRoute, AuthorizationStatus, AppRoute, TIMEOUT_SHOW_ERROR} from '../const';
 import {AuthData} from '../types/auth-data';
 import {UserData} from '../types/user-data';
 import {store} from './';
@@ -59,6 +59,7 @@ export const loginAction = createAsyncThunk<void, AuthData, {
     const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
     saveToken(token);
     dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    dispatch(redirectToRoute(AppRoute.Result));
   },
 );
 